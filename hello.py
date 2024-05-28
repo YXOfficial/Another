cb5b1fb2f4bdc30ba147b41122c8786c453f847b
YXOfficial <the666khoa@gmail.com>
2024-05-28 14:45:24 +0700

    f

--- hello.py
+++ hello.py
@@ -1,13 +1,41 @@
-from flask import Flask, request, render_template, make_response, send_from_directory
-import requests
+from flask import Flask, request, render_template, send_from_directory, url_for, session, redirect
+from flask_mail import Mail, Message
+import re
 import os
 from werkzeug.utils import secure_filename
+import mysql.connector
+import random
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Khoa12345@",
+  database='logininfo'
+)
+
+cursor = mydb.cursor()
+
+
 app = Flask(__name__)
+
+mail = Mail(app)
+
+# configuration of mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'dashdashgame@gmail.com'
+app.config['MAIL_PASSWORD'] = 'hmcwdwtehadifqiu'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 from flask import url_for
 basedir = os.path.abspath(os.path.dirname(__file__))
 UploadPath = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+
+
 
 @app.route('/uploaded_file', methods=['POST', 'GET'])
 def uploaded():
@@ -50,5 +78,84 @@ def uploaded():
 @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
     return send_from_directory(UploadPath, filename, as_attachment=True)
+
+@app.route('/Login', methods=['GET', 'POST'])
+def login():
+    message = ''
+    if request.method == 'POST' and 'gmail' in request.form and 'password' in request.form:
+        gmail = request.form['gmail']
+        password = request.form['password']
+        cursor.execute('SELECT * FROM LoginInfo WHERE GMAIL = %s AND PASSWORD = %s', (gmail, password, ))
+        account = cursor.fetchone()
+        if account:
+            # session['loggedin'] = True
+            # session['id'] = account['id']
+            # session['USERNAME'] = account['USERNAME']
+            message = 'Logged in successfully !'
+            return render_template('Login.html', message=message)
+        else:
+            message = 'Incorrect user and pass'
+
+    return render_template('Login.html', message=message)
+
+@app.route('/Register', methods=['GET', 'POST'])
+def Register():
+    message = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        name = request.form['username']
+        password = request.form['password']
+        gmail = request.form['gmail']
+
+        cursor.execute('SELECT * FROM logininfo WHERE GMAIL = %s', (gmail, ))
+        account = cursor.fetchone()
+        if account:
+            message = "gmail account already exists."
+            return render_template('Register.html', message=message)
+        else:
+            cursor.execute('INSERT INTO LoginInfo (USERNAME, PASSWORD, GMAIL) VALUES (%s, %s, %s)', (name, password, gmail, ))
+            mydb.commit()
+            message = "Created account."
+    return render_template('Register.html', message=message)
+
+@app.route('/PasswordReset/<token>/<gmail>', methods=['GET', 'POST'])
+def PasswordChanger(token, gmail):
+    message = ''
+    cursor.execute('USE logininfo;')
+    cursor.execute('SELECT * FROM logininfo WHERE GMAIL = %s AND TOKEN=%s', (gmail, token))
+    counter = cursor.fetchone()
+    if not counter:
+        return render_template('nope.html')
+    if request.method == 'POST' and 'password' in request.form:
+        passchange = request.form['password']
+        cursor.execute('UPDATE logininfo SET PASSWORD= %s WHERE GMAIL=%s', (passchange, gmail))
+        mydb.commit()
+        message = "changed pass"
+    return render_template('Passwordreset.html', message=message, token=token, gmail=gmail)
+
+@app.route('/Reset', methods=['GET', 'POST'])
+def Reset():
+    message = ''
+    if request.method == "POST":
+        gmail = request.form['gmail']
+        cursor.execute('USE logininfo;')
+        cursor.execute('SELECT * FROM logininfo WHERE GMAIL = %s', (gmail,))
+        account = cursor.fetchone()
+        if not account:
+            message = "Not exists."
+            return render_template('Reset.html', message=message)
+        else:
+            token = random.randrange(1, 9999)
+            cursor.execute('UPDATE LoginInfo SET Token = %s WHERE GMAIL = %s', (token, gmail))
+            mydb.commit()
+            msg = Message(
+                'Your token, please dont share anyone',
+                sender='noreply@gmail.com',
+                recipients=[gmail]
+            )
+            msg.body = ('http://127.0.0.1:5000/PasswordReset/{0}/{1}'.format(token, gmail))
+            mail.send(msg)
+            message = 'sent'
+    return render_template('Reset.html', message=message)
+
 
 app.run(debug=True)
